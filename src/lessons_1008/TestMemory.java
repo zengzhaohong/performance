8196fee936ec7b96ba5951ddd923c6b7f9f0f3b5
zengzhaohong <1115827740@qq.com>
2020-12-13 23:01:21 +0800

    1

---
+++ src/lessons_1008/TestMemory.java
@@ -0,0 +1,32 @@
+package lessons_1008;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author 11158
+ * 模拟list不释放，造成内存占用高
+ */
+
+public class TestMemory {
+
+	static List<Person> list = new ArrayList<>();
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 500000; i++) {
+			list.add(new Person());
+		}
+		try {
+			Thread.sleep(1000000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+class Person{
+	public String name = "zz";
+	public int age = 18;
+}
